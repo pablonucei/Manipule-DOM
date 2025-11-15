8ccce4c1e15268b5cc90743a3cc51aa895bca264
pablonucei <pablonucei@gmail.com>
2025-11-15 12:59:02 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+$('#botao-cancelar').click(function() {
+    $('form').slideUp();
+})
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val(); 
+        console.log(enderecoDaNovaImagem);
+        const novoItem = $('<li></li>');
+        $(`<img scr="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+               <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+               Ver imagem em tamanho real
+               </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+    })
+})
+
